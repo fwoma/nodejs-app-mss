ccf180910a9a36b1600b45d7dde3be44cb2ee15c
fwoma <136750885+fwoma@users.noreply.github.com>
2023-10-02 09:56:36 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://localhost:9000/',
+  serverUrl: 'http://54.237.224.63/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'Woma',
-	    'sonar.login': '846da14c5dd24dd8bd05d129f0ab6ec0dc9172bf',
+	    'sonar.login': '5d47b12a630ed19c0ab61dd519af2bc02f9fc1df',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
